9f6373c65eb0906bd46b7f531174fc7c8d51743e
Felly KANYIKI NGOY <fellyka@gmail.com>
2023-05-15 16:06:22 +0200

    Edit page implemented

---
+++ Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using fellyka.Data;
+using fellyka.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace fellyka.Pages.Categories
+{
+    public class Edit : PageModel
+    {
+        private readonly AppDbContext db;
+        private readonly ILogger<Create> _logger;
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public Edit(AppDbContext db,ILogger<Create> logger)
+        {
+            this.db = db;
+            _logger = logger;
+        }
+
+        public void OnGet(int id)
+        {
+             //Category = db.Categories.FirstOrDefault(x => x.Id == id);
+            //Category = db.SingleOrDefault(x => x.Id == id);
+           //Category = db.Categories.Where(x => x.Id == id).FirstOrDefault;
+            Category = db.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The DispalyPrder cannot exactly match the Name.");
+            }
+            if(ModelState.IsValid)
+            {
+            db.Categories.Update(Category);
+            await db.SaveChangesAsync();
+            return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
