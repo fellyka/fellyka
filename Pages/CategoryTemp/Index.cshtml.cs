4668391cc06f75d8429ed6f23546f8d7ab260ebd
Felly KANYIKI NGOY <fellyka@gmail.com>
2023-05-16 19:47:06 +0200

    CategoryTemp added (Razor Page using Entity Framework(CRUD))

---
+++ Pages/CategoryTemp/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using fellyka.Data;
+using fellyka.Model;
+
+namespace fellyka.Pages.CategoryTemp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly fellyka.Data.AppDbContext _context;
+
+        public IndexModel(fellyka.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Categories != null)
+            {
+                Category = await _context.Categories.ToListAsync();
+            }
+        }
+    }
+}
